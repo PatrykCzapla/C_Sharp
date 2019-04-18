17f412743fba02acd857402b3f301c1f4c112e82
PatrykCzapla <49670317+PatrykCzapla@users.noreply.github.com>
2019-04-18 17:56:05 +0200

    Add files via upload

---
+++ Lab11/ChristmasFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p3z
+{
+    static class ChristmasFactory
+    {
+        public static bool IsPrime(long a)
+        {
+            for (long i = 2; i <= Math.Ceiling(Math.Sqrt(a)); i++)
+            {
+                if (a % i == 0) return false;
+            }
+            return true;
+        }
+        public static long NumberFactorization(long a)
+        {
+            try
+            {
+                if(IsPrime(a))throw new IncorrectFactorizationException(IncorrectFactorizationException.Exceptiontype.IsPrime);
+                for (long i = 2; i < Math.Ceiling(Math.Sqrt(a)); i++)
+                {
+                    if ((a % i == 0) && IsPrime(i))
+                        if(!IsPrime((long)a/i)) throw new IncorrectFactorizationException(IncorrectFactorizationException.Exceptiontype.MoreThan2Factors);
+                        else return i;
+                }
+                return -1;
+            }
+            catch(IncorrectFactorizationException w) when (w.type== IncorrectFactorizationException.Exceptiontype.IsPrime)
+            {
+                //Console.WriteLine("IsPrime");
+                return a;
+            }
+            catch (IncorrectFactorizationException w) when (w.type == IncorrectFactorizationException.Exceptiontype.MoreThan2Factors)
+            {
+                //Console.WriteLine("MoreThan2Factors");
+                return 3;
+            }
+
+        }
+        public static async Task<List<(int,long)>> ManageLetters(List<Letter> lista)
+        {
+            List<(int, long)> result = new List<(int, long)>();
+
+            foreach (var elem in lista)
+            {
+                Task<long> factor = Task<long>.Run(() => NumberFactorization(elem.Number));
+                long r = await factor;
+                result.Add((elem.Id, r));
+            }
+
+            return result;
+        }
+        
+    }
+    public class IncorrectFactorizationException : SystemException
+    {
+        public enum Exceptiontype { IsPrime, MoreThan2Factors};
+        public Exceptiontype type;
+        public IncorrectFactorizationException(Exceptiontype a)
+        {
+            type = a;
+        }
+    }
+}
---
+++ Lab11/SantaClause.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace p3z
+{
+	class SantaClause
+	{
+		Random rand = new Random(7);
+
+		public List<(int, string)> decodedLetters = new List<(int, string)>(); //lista par: (identyfikator listu, zdekodowane imię dziecka)
+
+        private List<Task> tasks = new List<Task>(); 
+
+		private string DecodeChildName(long number)
+		{
+			Dictionary<long, string> childrenList = new Dictionary<long, string>() { { 3, "Marek" },
+				{ 195421207, "Andrzej" }, { 32416189859, "Agnieszka" }, { 247531, "Kamil" },
+				{ 4743953, "Dżesika" }, { 1536839, "Zuzia" }, { 21887587, "Brajanek" },
+				{ 1503401, "Ola" }, { 12653723, "Janusz" }, { 522211, "Grażyna" }, {59464991, "Grześ" },
+				{ 86281861, "Tomek" } };
+
+			if (childrenList.ContainsKey(number))
+				return childrenList[number];
+			else
+				return "UNKNOWN";
+		}
+
+		public void PrepareForChristmas(List<Letter> letters)
+		{
+			for (int day = 19; day < 24; day++)
+			{
+                tasks.Add( SendLettersToFactory(letters, day) );
+
+                GoSleep();
+
+                //WhatAbout(letters);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Ho! Ho! Ho! Wesołych świąt!");
+		}
+        //public void WhatAbout(List<Letter> letters)
+        //{
+        //    lock(decodedLetters)
+        //    {
+        //        if()
+        //    }
+        //}
+		public void GoSleep()
+		{
+			Console.WriteLine("Mikołaj śpi");
+			System.Threading.Thread.Sleep(1000);
+		}
+
+        public async Task SendLettersToFactory(List<Letter> list, int day)
+        {
+            List<Letter> tmplist = new List<Letter>();
+            
+
+            foreach (var v in list)
+            {
+                if (v.ReceivedDay == day) tmplist.Add(v);
+            }
+
+            Console.WriteLine("Wysłanie listów do fabryki z {0} grudnia", day);
+            List<(int, long)> results = await (ChristmasFactory.ManageLetters(tmplist));
+
+            Console.WriteLine("Przetworzono wszystkie listy z {0} grudnia", day);
+
+            foreach (var elem in results)
+            {
+                //Console.WriteLine(elem.Item2);
+                lock(decodedLetters)
+                {
+                    decodedLetters.Add(((int)(elem.Item1), DecodeChildName(elem.Item2)));
+                }
+                
+            }
+
+        }
+
+    }
+
+}
