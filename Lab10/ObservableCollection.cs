aa6db1fc62c9bfb7850f93a7ee4ee59355791407
PatrykCzapla <49670317+PatrykCzapla@users.noreply.github.com>
2019-04-18 17:55:32 +0200

    Add files via upload

---
+++ Lab10/ObservableCollection.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11b
+{
+    class ObservableCollection : IObservableCollection
+    {
+        List<object> lista = new List<object>();
+        public string Name
+        {
+            get;
+        }
+        public ObservableCollection(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public event EventHandler<CollectionCahngedEventArgs> CollectionCahnged = null;
+        void fun2 (object sender, CollectionCahngedEventArgs arg)
+        {
+           
+        }
+        public void Add(object value)
+        {
+                    
+            if(value.GetType()==(Type)typeof(NotifingObject))
+            {
+                
+            }
+            if (value != null)
+            {
+                lista.Add(value);
+                if(value is NotifingObject)    
+                if (CollectionCahnged != null) CollectionCahnged.Invoke(this, new CollectionCahngedEventArgs(CollectionOperation.Add, value));
+
+            }
+        }
+
+        public void Remove(object value)
+        { 
+            if (value != null && lista != null)
+            {
+                lista.Remove(value);
+                if (CollectionCahnged != null) CollectionCahnged.Invoke(this, new CollectionCahngedEventArgs(CollectionOperation.Remove, value));
+            }
+                
+        }
+
+        
+    }
+    public class NotifingObject : IChangeNotifing
+    {
+        public event EventHandler NameChanged;
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+
+                name = Name;
+                NameChanged.Invoke(this, new CollectionCahngedEventArgs(CollectionOperation.ValueChanged, this));
+            }
+        }
+      
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ Lab10/Watcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11b
+{
+    interface IWatcher
+    {
+        void Watch(IObservableCollection icn);
+        void StopWatching(IObservableCollection icn);
+    }
+
+    class SimpleWatcher : IWatcher
+    {
+        EventHandler<CollectionCahngedEventArgs> func = (object sender, CollectionCahngedEventArgs e) =>
+        {
+            Console.WriteLine("Collection changed");
+        };
+
+        public void StopWatching(IObservableCollection icn)
+        {
+            icn.CollectionCahnged -= func;
+        }
+
+        public void Watch(IObservableCollection icn)
+        {
+            icn.CollectionCahnged += func;
+        }
+    }
+    class SmartWatcher : IWatcher
+    {
+        EventHandler<CollectionCahngedEventArgs> func = (object sender, CollectionCahngedEventArgs e) =>
+        {
+            Console.WriteLine("Collection: "+ (sender as ObservableCollection).Name + " changed, value: " +  e.Value + " was " + e.Operation+"ed");
+        };
+
+        public void StopWatching(IObservableCollection icn)
+        {
+            icn.CollectionCahnged -= func;
+        }
+
+        public void Watch(IObservableCollection icn)
+        {
+            icn.CollectionCahnged += func;
+        }
+    }
+}
